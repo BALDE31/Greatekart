509a2d247ad50ff0a848a974a6b6fdf1f2140fac
BALDE31 <mount.edlab@gmail.com>
2021-08-06 16:19:07 +0200

    Make storage Page

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from greatkart.settings import MEDIA_ROOT
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+#import store.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('store/', include('store.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
