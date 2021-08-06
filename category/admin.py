5b57bb309300b7163735c2f5577521f64b21e438
BALDE31 <mount.edlab@gmail.com>
2021-08-06 02:50:34 +0200

    Pre-populate Category field

--- category/admin.py
+++ category/admin.py
@@ -2,6 +2,37 @@ from django.contrib import admin
 from .models import Category
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)}
 
-admin.site.register(Category)
+    ordering = ['id']
+    list_display = ('category_name', 'slug',)
+    
+    """
+    readonly_fields = ('last_login', 'date_joined')
+    filter_horizontal = ()
+    list_filter = ['last_name', 'username']
+    search_fields = ['last_name', 'first_name', 'email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('last_name', 'first_name', 'username')}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superadmin', 'is_admin')}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+    """
+    
+
+
+#admin.site.register(Category)
 
