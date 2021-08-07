710508300cdf2511acb9120cae578b73b86abdd0
BALDE31 <mount.edlab@gmail.com>
2021-08-07 18:44:45 +0200

    Maked context processor for displaying Categories or Navbar

--- store/views.py
+++ store/views.py
@@ -11,6 +11,7 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
         product_count = products.count()
