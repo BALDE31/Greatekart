aa2cf74aab0c87c6891c72724ca163625a1931ed
BALDE31 <mount.edlab@gmail.com>
2021-08-08 05:10:52 +0200

    Implement Product Detail Url and Design

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from store.models import Product
 from category.models import Category
 
 
-def store(request, category_slug=None):
+def store(request, category_slug):
     categories = None
     products = None
     product_count = None
@@ -22,3 +22,26 @@ def store(request, category_slug=None):
     }
 
     return render(request, 'store/store.html', context)
+
+def product_detail(request, category_slug, product_slug):
+    
+    """
+    categories = None
+    products = None
+    product_count = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    """
+
+    return render(request, 'store/product_detail.html')
