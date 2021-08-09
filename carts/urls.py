792fec6ebf27dff97e2d4964b3f58cd8fbe38fe5
BALDE31 <mount.edlab@gmail.com>
2021-08-09 17:07:01 +0200

    Drecremente & Remove item from CartItem

--- carts/urls.py
+++ carts/urls.py
@@ -5,6 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.cart, name="cart"),
-    path('add_card/<int:product_id>/', views.add_cart, name="add_cart"),
+    path('add_cart/<int:product_id>/', views.add_cart, name="add_cart"),
+    path('remove_cart/<int:product_id>/', views.remove_cart, name="remove_cart"),
+    path('delete_cart_item/<int:product_id>/', views.delete_cart_item, name="delete_cart_item"),
     #path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name="product_detail"),
 ]
+ 
--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ObjectDoesNotExist
 from carts.models import Cart
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from store.models import Product
 from .models import Cart, CartItem
 from django.http import HttpResponse
@@ -39,6 +39,25 @@ def add_cart(request, product_id):
     return redirect('cart')
 
 
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+
+def delete_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete() 
+    return redirect('cart')
+
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
@@ -47,7 +66,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
 
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-        tax = (2* total)/100
+        tax = (2 * total)/100
         grand_total = total + tax
     except ObjectDoesNotExist:
         pass
