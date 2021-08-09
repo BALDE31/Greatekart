59e0f42caebec07973175b8c978f1155900b3628
BALDE31 <mount.edlab@gmail.com>
2021-08-09 02:03:46 +0200

    Cart and cart item model

--- carts/admin.py
+++ carts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
 # Register your models here.
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ()
+    #prepopulated_fields = {'slug': ('product_name',)}
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ()
+    #prepopulated_fields = {'slug': ('product_name',)}
